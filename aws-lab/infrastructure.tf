4be9725f54e71abf24331200848f6716ea1bd097
Dave Randall <dave@randall-it.uk>
2017-05-29 14:17:11 +0100

    Added support for multiple environments

--- aws-lab/infrastructure.tf
+++ aws-lab/infrastructure.tf
@@ -2,21 +2,21 @@ provider "aws" {
   region = "${var.region}"
 }
 
-resource "aws_vpc" "lab" {
+resource "aws_vpc" "vpc" {
   cidr_block = "10.100.0.0/16"
 
   tags {
-    Name = "lab"
+    Name = "${terraform.env}"
   }
 }
 
-resource "aws_subnet" "public-a1" {
+resource "aws_subnet" "subnet-public-a1" {
   availability_zone = "${var.region}a"
   cidr_block = "10.100.0.0/23"
-  vpc_id = "${aws_vpc.lab.id}"
+  vpc_id = "${aws_vpc.vpc.id}"
 
 
   tags {
-    Name = "public-a1"
+    Name = "${terraform.env}-public-a1"
   }
 }
