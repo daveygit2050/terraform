48c6ef9665fdef5dcb83d9aef2e704dab69c257b
Dave Randall <dave@randall-it.uk>
2018-02-17 17:15:31 +0000

    Associating private subnets with NAT gateways

--- aws-lab/routing.tf
+++ aws-lab/routing.tf
@@ -1,3 +1,17 @@
+resource "aws_route_table" "private" {
+  count = 3
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = "${aws_nat_gateway.ng.*.id[count.index]}"
+  }
+
+  tags {
+    Name = "${var.name}-private-${element(var.az-suffixes, count.index)}"
+  }
+}
+
 resource "aws_route_table" "public" {
   vpc_id = "${aws_vpc.vpc.id}"
 
@@ -9,6 +23,12 @@ resource "aws_route_table" "public" {
   tags {
     Name = "${var.name}-public"
   }
+}
+
+resource "aws_route_table_association" "private" {
+  count = 3
+  subnet_id = "${aws_subnet.private.*.id[count.index]}"
+  route_table_id = "${aws_route_table.private.*.id[count.index]}"
 }
 
 resource "aws_route_table_association" "public" {
