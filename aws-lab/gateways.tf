f6458b084f1ab947e38bfa9f646b1521dc49530e
Dave Randall <dave@randall-it.uk>
2018-02-17 17:09:00 +0000

    Adding NAT gateways

--- aws-lab/gateways.tf
+++ aws-lab/gateways.tf
@@ -5,3 +5,22 @@ resource "aws_internet_gateway" "ig" {
     Name = "${var.name}"
   }
 }
+
+resource "aws_eip" "ng" {
+  count = 3
+  vpc = true
+
+  tags {
+    Name = "${var.name}-nat-${element(var.az-suffixes, count.index)}"
+  }
+}
+
+resource "aws_nat_gateway" "ng" {
+  allocation_id = "${aws_eip.ng.*.id[count.index]}"
+  count = 3
+  subnet_id = "${aws_subnet.public.*.id[count.index]}"
+
+  tags {
+    Name = "${var.name}-${element(var.az-suffixes, count.index)}"
+  }
+}
