c4cb8f7bd7dbd3f6cb934cdecf7be31c1c68dbb5
Dave Randall <dave@randall-it.uk>
2018-02-17 16:52:33 +0000

    Public route table now associated with public subnets

--- aws-lab/routing.tf
+++ aws-lab/routing.tf
@@ -10,3 +10,9 @@ resource "aws_route_table" "public" {
     Name = "${var.name}-public"
   }
 }
+
+resource "aws_route_table_association" "public" {
+  count = 3
+  subnet_id = "${aws_subnet.public.*.id[count.index]}"
+  route_table_id = "${aws_route_table.public.id}"
+}
