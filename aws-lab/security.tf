977cd3f9413dd618e9f4abf420800df095298201
Dave Randall <dave@randall-it.uk>
2018-02-17 17:47:38 +0000

    New security groups for private servers

--- aws-lab/security.tf
+++ aws-lab/security.tf
@@ -23,3 +23,29 @@ resource "aws_security_group" "bastions" {
     Name = "${var.name}-bastions"
   }
 }
+
+resource "aws_security_group" "private" {
+  description = "Applied to ${var.name} private servers"
+  name = "${var.name}-private"
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  ingress {
+    description = "Allow inbound SSH from bastion servers"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    security_groups = ["${aws_security_group.bastions.id}"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "${var.name}-private"
+  }
+}
